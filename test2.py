14c394c8bfea75cf41ed787ff27dc72b1c901895
venusaur <38409968+venusaur@users.noreply.github.com>
2023-04-02 13:38:46 -0700

    fixed input error, need to see if this actually works on instrument

--- test2.py
+++ test2.py
@@ -10,7 +10,6 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plt
 from tkinter import *
-from functions import *
 
 
 #--------------------------------------#
@@ -56,101 +55,119 @@ setPointsInput = Entry(root, width=10)
 setPointsInput.grid(row=4, column=1, pady=10)
 
 
-
 #--------------------------------------#
                 #User inputs#
 #--------------------------------------#
-startfreq = int(startFreqInput.get())
-print(startfreq) # Debug statement
 
-endfreq = int(endFreqInput.get())
-print(endfreq) # Debug Statment
 
-freqstep = int (freqStepInput.get())
-print(freqstep) # Debug Statement
+startfreq = int(startFreqInput.get()) if startFreqInput.get() else 0
 
-averages = int(setAvgInput.get())
-print(averages) #Debug statment
+endfreq = int(endFreqInput.get()) if endFreqInput.get() else 0
 
-numpts = int(setPointsInput.get())
-print(numpts) #debug statment
+freqstep = int (freqStepInput.get()) if freqStepInput.get() else 0
 
-runButton = Button(root, text="RUN SCAN")
+averages = int(setAvgInput.get()) if setAvgInput.get() else 0
+
+numpts = int(setPointsInput.get()) if setPointsInput.get() else 0
+
+
+if startfreq or endfreq or freqstep or averages or numpts == 0:
+    print("There is a value = 0")
+
+
+
+def process(startfreq, endfreq, freqstep, averages, numpts):
+
+        #--------------------------------------#
+                        #User inputs#
+        #--------------------------------------#
+
+
+        startfreq = int(startFreqInput.get()) if startFreqInput.get() else 0
+        print(startfreq) # Debug statement
+
+        endfreq = int(endFreqInput.get()) if endFreqInput.get() else 0
+        print(endfreq) # Debug Statment
+
+        freqstep = int (freqStepInput.get()) if freqStepInput.get() else 0
+        print(freqstep) # Debug Statement 
+
+        averages = int(setAvgInput.get()) if setAvgInput.get() else 0
+        print(averages) #Debug statment
+
+        numpts = int(setPointsInput.get()) if setPointsInput.get() else 0
+        print(numpts) #debug statment
+
+        #--------------------------------------#
+                #Background Calculations#
+        #--------------------------------------#
+        
+        endfreq += freqstep
+        samprate = (numpts*startfreq)*averages # Might want to display this value later
+        numptTot = numpts*averages # Might want to display this value aswell
+
+        data = np.empty([numptTot,(endfreq-startfreq)])
+        names = ['Frequency %d' % (startfreq)]
+
+        for x in np.arange (1, ((endfreq-startfreq)), freqstep):
+                names.append("Frequency %d" % (x+startfreq))         
+       
+        data = pd.DataFrame(data, columns = names)
+        np.resize(data, data.shape)
+
+        #--------------------------------------#
+                #DAQmx Initialization#
+        #--------------------------------------#
+        CO1 = nidaqmx.Task()    #Initializes Counter Output
+        CO1.co_channels.add_co_pulse_chan_time("Dev1/ctr0") # This is where I want to investigate with rp\
+        CO1.timing.cfg_implicit_timing(sample_mode=AcquisitionType.CONTINUOUS)
+        cw = CounterWriter(CO1.out_stream, True)
+
+        CO1.start()     #Starts Counter
+
+        AI1 = nidaqmx.Task()    #Initializes Analog Input on Channel 0 (Differential measurement)
+        AI1.ai_channels.add_ai_voltage_chan("Dev1/ai0") # Investigate this as well
+        AI1.timing.cfg_samp_clk_timing(samprate, sample_mode=AcquisitionType.FINITE, samps_per_chan = numptTot)    # Sets timing to collect total number of points requested
+        AI1.triggers.start_trigger.cfg_dig_edge_start_trig("PFI12")
+
+
+        #--------------------------------------#
+                #DAQmx Data Collection#
+        #--------------------------------------#
+        startTime = datetime.datetime.now() # Need to output start time
+
+        for x in np.arange (startfreq, endfreq, freqstep):  # Iterates over each frequency to collect, collecting numptstot at each step after triggering
+                cw.write_one_sample_pulse_frequency(frequency=x, duty_cycle=0.5)
+                data['Frequency %d' %(x)] = AI1.read(number_of_samples_per_channel = numptTot)
+
+        endTime = datetime.datetime.now() # Need to output End Time
+
+        timeTot = endTime - startTime # Need to output total time
+
+        CO1.stop()
+        CO1.close()
+
+        AI1.stop()
+        AI1.close()
+
+        #--------------------------------------#
+                #Data Processing#
+        #--------------------------------------#
+        avg = []                # initializes empty array to store average values in.
+        for column in data:     # iterates over each column in the data dataframe to find average
+                avg.append(data[column].mean())
+
+
+        #print(avg)     # Might want to display this value aswell
+
+
+        output = pd.DataFrame(np.arange(startfreq, endfreq, freqstep), columns = ['Frequency']) #Creates new dataframe for the final output, populates with the frequencies tested
+        output['Average Signal'] = avg   # Output this value as well
+
+
+
+
+runButton = Button(root, text="RUN SCAN", command = lambda: process(startfreq, endfreq, freqstep, averages, numpts))
 runButton.grid(row=2, column=5, padx=50)
-
-print(startfreq)
-
-
-
-
-
-#--------------------------------------#
-        #Background Calculations#
-#--------------------------------------#
-endfreq += freqstep
-samprate = (numpts*startfreq)*averages # Might want to display this value later
-numptTot = numpts*averages # Might want to display this value aswell
-
-data = np.empty([numptTot,(endfreq-startfreq)])
-names = ['Frequency %d' % (startfreq)]
-
-for x in np.arange (1, ((endfreq-startfreq)), freqstep):
-    names.append("Frequency %d" % (x+startfreq))         # Can I add a prgress bar for this in UI
-
-data = pd.DataFrame(data, columns = names)
-
-
-
-#--------------------------------------#
-        #DAQmx Initialization#
-#--------------------------------------#
-CO1 = nidaqmx.Task()    #Initializes Counter Output
-CO1.co_channels.add_co_pulse_chan_time("Dev1/ctr0") # This is where I want to investigate with rp\
-CO1.timing.cfg_implicit_timing(sample_mode=AcquisitionType.CONTINUOUS)
-cw = CounterWriter(CO1.out_stream, True)
-
-CO1.start()     #Starts Counter
-
-AI1 = nidaqmx.Task()    #Initializes Analog Input on Channel 0 (Differential measurement)
-AI1.ai_channels.add_ai_voltage_chan("Dev1/ai0") # Investigate this as well
-AI1.timing.cfg_samp_clk_timing(samprate, sample_mode=AcquisitionType.FINITE, samps_per_chan = numptTot)    # Sets timing to collect total number of points requested
-AI1.triggers.start_trigger.cfg_dig_edge_start_trig("PFI12")
-
-
-#--------------------------------------#
-        #DAQmx Data Collection#
-#--------------------------------------#
-startTime = datetime.datetime.now() # Need to output start time
-
-for x in np.arange (startfreq, endfreq, freqstep):  # Iterates over each frequency to collect, collecting numptstot at each step after triggering
-    cw.write_one_sample_pulse_frequency(frequency=x, duty_cycle=0.5)
-    print(x)
-    data['Frequency %d' %(x)] = AI1.read(number_of_samples_per_channel = numptTot)
-
-endTime = datetime.datetime.now() # Need to output End Time
-
-timeTot = endTime - startTime # Need to output total time
-
-CO1.stop()
-CO1.close()
-
-AI1.stop()
-AI1.close()
-
-#--------------------------------------#
-        #Data Processing#
-#--------------------------------------#
-avg = []                # initializes empty array to store average values in.
-for column in data:     # iterates over each column in the data dataframe to find average
-    avg.append(data[column].mean())
-
-
-#print(avg)     # Might want to display this value aswell
-
-
-output = pd.DataFrame(np.arange(startfreq, endfreq, freqstep), columns = ['Frequency']) #Creates new dataframe for the final output, populates with the frequencies tested
-output['Average Signal'] = avg   # Output this value as well
-
-
 root.mainloop()
 
