0b0bc7a05fa66eab3325821f2f2653f0cb70d724
SG <38409968+venusaur@users.noreply.github.com>
2023-05-23 09:37:11 -0700

    Update ftims.py

--- pico/ftims.py
+++ pico/ftims.py
@@ -1,9 +1,9 @@
-import machine
+from machine import I2C, Pin
 import utime
 
 I2C_SLAVE_ADDRESS = 0x12  # I2C slave address of the Teensy
 
-i2c = machine.I2C(0, scl=machine.Pin(1), sda=machine.Pin(0), freq=400000)  # Create an I2C object with the appropriate pins and frequency
+i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=100000)  # Create an I2C object with the appropriate pins and frequency
 
 while True:
     # Send a value of 1 to turn on the LED
@@ -15,3 +15,11 @@ while True:
     i2c.writeto(I2C_SLAVE_ADDRESS, bytes([0]))
 
     utime.sleep(1)  # Wait for 1 second
+
+    # Request the LED status from the Teensy
+    i2c.writeto(I2C_SLAVE_ADDRESS, bytes([2]))
+    response = i2c.readfrom(I2C_SLAVE_ADDRESS, 1)
+
+    led_status = int.from_bytes(response, 'big')
+
+    print("LED Status:", led_status)
