c80e253dcd84f730af795f8370c1be64e2303b95
SG <38409968+venusaur@users.noreply.github.com>
2023-05-22 17:32:04 -0700

    Update ftims.py

--- pico/ftims.py
+++ pico/ftims.py
@@ -2,7 +2,7 @@ import machine
 import utime
 
 # Configure UART
-uart = machine.UART(0, baudrate=9600, bits=8, parity=None, stop=1, tx=0, rx=1)
+uart = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1))
 
 # Send data to the Teensy
 uart.write("Hello Teensy!")
@@ -10,3 +10,4 @@ uart.write("Hello Teensy!")
 while True:
     # Additional code or operations
     utime.sleep(1)
+
