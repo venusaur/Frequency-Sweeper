e6a48e770c329658eea6a8c927f158818b645337
SG <38409968+venusaur@users.noreply.github.com>
2023-05-22 17:29:51 -0700

    Update ftims.py

--- pico/ftims.py
+++ pico/ftims.py
@@ -1,23 +1,12 @@
 import machine
 import utime
 
-# Configure SPI
-spi = machine.SPI(0)
-spi.init(baudrate=1000000, polarity=0, phase=0)  # Adjust the baudrate, polarity, and phase as needed
+# Configure UART
+uart = machine.UART(0, baudrate=9600, bits=8, parity=None, stop=1, tx=0, rx=1)
 
-# Define CS (Chip Select) pin
-cs_pin = machine.Pin(5, machine.Pin.OUT)  # Use any available GPIO pin
+# Send data to the Teensy
+uart.write("Hello Teensy!")
 
-# Function to send and receive data
-def spi_transfer(data):
-    cs_pin.off()  # Activate the CS pin
-    result = spi.write_readinto(data)
-    cs_pin.on()  # Deactivate the CS pin
-    return result
-
-# Example usage
 while True:
-    data_to_send = bytearray([0x01, 0x02, 0x03])  # Example data to send
-    received_data = spi_transfer(data_to_send)
-    print("Received data:", received_data)
+    # Additional code or operations
     utime.sleep(1)
